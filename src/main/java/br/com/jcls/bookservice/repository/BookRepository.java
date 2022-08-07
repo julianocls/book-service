c75395a1ad48b6bed1b3ecdb7a4ab4012ec666ed
Juliano C L Santos <foxports@hotmail.com>
2022-08-07 19:01:02 -0300

    Database configure

--- src/main/java/br/com/jcls/bookservice/repository/BookRepository.java
+++ src/main/java/br/com/jcls/bookservice/repository/BookRepository.java
@@ -1,12 +1,9 @@
 package br.com.jcls.bookservice.repository;
 
 import br.com.jcls.bookservice.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-//@Repository
-public interface BookRepository  {
-
-    Optional<Book> findById(Long id);
-
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/br/com/jcls/bookservice/service/BookService.java
+++ src/main/java/br/com/jcls/bookservice/service/BookService.java
@@ -1,11 +1,10 @@
 package br.com.jcls.bookservice.service;
 
 import br.com.jcls.bookservice.model.Book;
+import br.com.jcls.bookservice.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Service
 public class BookService {
@@ -13,17 +12,20 @@ public class BookService {
     @Autowired
     private Environment environment;
 
-    //@Autowired
-    //private BookRepository repository;
+    @Autowired
+    private BookRepository repository;
 
     public Book findBook(Long id, String currency) {
 
-        //var book = repository.findById(id);
-        //if (!book.isPresent()) { throw new RuntimeException("Book not found!"); }
+        var bookOptional = repository.findById(id);
+        if (!bookOptional.isPresent()) { throw new RuntimeException("Book not found!"); }
+        var book = bookOptional.get();
 
         var port = environment.getProperty("local.server.port");
+        book.setCurrency(currency);
+        book.setEnvironment(port);
 
-        return new Book(1L, "Negel Poulton", new Date(), Double.valueOf(13.7), "Docker Deep Dive", currency, port);
+        return book;
     }
 
 }
