2b29fe2ae0865ba08136e462d5f14e284b68f85d
Juliano C L Santos <foxports@hotmail.com>
2022-08-09 00:56:48 -0300

    Configure redis cache timeout

--- src/main/java/br/com/jcls/bookservice/repository/BookRepository.java
+++ src/main/java/br/com/jcls/bookservice/repository/BookRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Cacheable( cacheNames = "Book", key = "#id")
+    @Cacheable( cacheNames = "book-expire")
     Optional<Book> findById(Long id);
 
 }
